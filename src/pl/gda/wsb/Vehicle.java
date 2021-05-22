998b5ef7105a2d6e47c9b38c9624646c68f852d5
Remek137 <Remeq137@gmail.com>
2021-05-22 12:00:56 +0200

    Zad 1-4

---
+++ src/pl/gda/wsb/Vehicle.java
@@ -0,0 +1,11 @@
+package pl.gda.wsb;
+
+public abstract class Vehicle {
+    String nrRejestracyjny;
+    Long nrVIN;
+    String color;
+    Double price;
+    Double spalanie;
+    String gasTankState;
+    int mileage;
+}
